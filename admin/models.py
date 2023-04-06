ec8daf7a5fff6f8d667e4bcf9bbccde637d5ef31
Lily Appely <lappee@yahoo.com>
2023-04-06 09:38:02 +0300

    [ADD] :: AppAdmin table view added to admin-panel

--- admin/models.py
+++ admin/models.py
@@ -1,20 +1,23 @@
 from sqladmin import ModelView
 
-from db.models import Campus, Room, Client, ClientInfo
+from db import models
 
 class FullAdminBase(ModelView):
     can_create = True
     can_edit = True
     can_delete = True
 
-class CampusAdmin(FullAdminBase, model=Campus):
+class CampusAdmin(FullAdminBase, model=models.Campus):
     pass
 
-class RoomAdmin(FullAdminBase, model=Room):
+class RoomAdmin(FullAdminBase, model=models.Room):
     pass
 
-class ClientAdmin(FullAdminBase, model=Client):
+class ClientAdmin(FullAdminBase, model=models.Client):
     pass
 
-class ClientInfoAdmin(FullAdminBase, model=ClientInfo):
+class ClientInfoAdmin(FullAdminBase, model=models.ClientInfo):
+    pass
+
+class AppAdminAdmin(FullAdminBase, model=models.AppAdmin):
     pass
--- main.py
+++ main.py
@@ -7,12 +7,11 @@ import uvicorn
 
 from settings import settings
 from db.setup import db_engine
-from admin.models import CampusAdmin, RoomAdmin, ClientAdmin, ClientInfoAdmin 
-from admin.auth import AdminAuth 
+from admin import models as m, auth as a 
 from router import router
 
 app = FastAPI()
-admin = Admin(app, db_engine, authentication_backend=AdminAuth(
+admin = Admin(app, db_engine, authentication_backend=a.AdminAuth(
     secret_key=settings.admin_passwd))
 
 @app.on_event("startup")
@@ -26,10 +25,11 @@ async def shutdown():
 app.include_router(router)
 
 # __ Admin Views __
-admin.add_view(CampusAdmin)
-admin.add_view(RoomAdmin)
-admin.add_view(ClientAdmin)
-admin.add_view(ClientInfoAdmin)
+admin.add_view(m.CampusAdmin)
+admin.add_view(m.RoomAdmin)
+admin.add_view(m.ClientAdmin)
+admin.add_view(m.ClientInfoAdmin)
+admin.add_view(m.AppAdminAdmin)
 
 if __name__ == "__main__":
     uvicorn.run(app, host="localhost", port=settings.serve_port)
